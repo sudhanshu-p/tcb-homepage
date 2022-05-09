6fffaf6951e2f92c743d6a3ada513aa1dacab168
Destux <pandeysv@rknec.edu>
2022-05-09 22:07:03 +0530

    Initial Commit with the basic structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import logo from './logo.jpg';
+import Burger from './burger.png'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <span className="sl">
+          <span className="signup">Sign Up</span>
+          <span className="login">Login</span>
+        </span>
+        <h1>The CodeBreakers</h1>
+        <button className='Menu-button'>
+        <img src={Burger} className="Menu-icon" alt="Menu-icon" width="50vh" height="50vh"/>
+        </button>
       </header>
+      <div className="App-body">
+        <div className="logo">
+          <img src={logo} className="body-logo" alt="The CodeBreakers Logo"/>
+        </div>
+        <div className="body-text">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim ex itaque consequuntur aliquam voluptatem, assumenda distinctio eligendi accusamus perferendis. Dicta minus voluptatem ex fugit, saepe maxime aut qui nulla consequatur.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
